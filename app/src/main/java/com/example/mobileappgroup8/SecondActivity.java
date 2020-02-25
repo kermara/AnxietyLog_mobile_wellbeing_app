f9d4d0696eba2ff14a1a09c0d5a4bdb728c2b13b
Pauli Vuolle-Apiala <pauli.vuolle-apiala@metropolia.fi>
2020-02-25 22:20:45 +0200

    testi

---
+++ app/src/main/java/com/example/mobileappgroup8/SecondActivity.java
@@ -0,0 +1,12 @@
+package com.example.mobileappgroup8;
+
+import android.os.Bundle;
+
+public class SecondActivity extends MainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
