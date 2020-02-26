5fde9aa28b742c55cd3963e620f769493569c195
Pauli Vuolle-Apiala <pauli.vuolle-apiala@metropolia.fi>
2020-02-26 11:32:17 +0200

    testi

--- app/src/main/java/com/example/mobileappgroup8/SecondActivity.java
+++ app/src/main/java/com/example/mobileappgroup8/SecondActivity.java
@@ -1,6 +1,9 @@
 package com.example.mobileappgroup8;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends MainActivity {
 
@@ -8,5 +11,15 @@ public class SecondActivity extends MainActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity);
+
+        Button b = (Button)findViewById(R.id.start_button);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent quizActivityIntent = new Intent(SecondActivity.this, QuizActivity.class);
+                startActivity(quizActivityIntent);
+            }
+        });
     }
 }
