841f2bba02f83483fc46b39985634d323b23349c
PauliVu <p4u11v4uusi1>
2020-02-29 11:14:00 +0200

    asddd

--- app/src/main/java/com/example/mobileappgroup8/QuizActivity.java
+++ app/src/main/java/com/example/mobileappgroup8/QuizActivity.java
@@ -3,6 +3,8 @@ package com.example.mobileappgroup8;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import java.util.Arrays;
@@ -17,11 +19,19 @@ public class QuizActivity extends SecondActivity {
     private String[] questions = {"Feeling nervous, anxious, or on edge", "kysymys 2", "kysymys 3", "kysymys 4", "kysymys 5 ", "kysymys 6", "kysymys 7"};
     private int[][] answeredQuestions = new int[7][2];
     private int questionNumber = 0;
+    private int points = 0;
+    TextView questionTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.quiz_activity);
+
+        RadioGroup radioGroup1 = findViewById(R.id. radio_group1);
+        RadioButton radio1 = findViewById(R.id. radio_button_1);
+        RadioButton radio2 = findViewById(R.id. radio_button_2);
+        RadioButton radio3 = findViewById(R.id. radio_button_3);
+        RadioButton radio4 = findViewById(R.id. radio_button_4);
 
         final TextView questionTV = findViewById(R.id. text_view_question);
         questionTV.setText(questions[questionNumber]);
@@ -35,8 +45,7 @@ public class QuizActivity extends SecondActivity {
                 } else {
                     if(!(Arrays.asList(answeredQuestions).contains(questionNumber))) {
                         answeredQuestions[questionNumber][0] = questionNumber;
-                        questionNumber++;
-                        questionTV.setText(questions[questionNumber]);
+                        questionIncrement();
                     }
                 }
             }
@@ -52,5 +61,9 @@ public class QuizActivity extends SecondActivity {
                 }
             }
         });
+    }
+    protected void questionIncrement(){
+        questionNumber++;
+        questionTV.setText(questions[questionNumber]);
     }
 }
