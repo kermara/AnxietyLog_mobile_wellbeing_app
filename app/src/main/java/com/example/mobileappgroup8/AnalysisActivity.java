e08a849cc9862d9e1152f6716b6b7cdcd64baf0c
irina <irinakon@metropolia.fi>
2020-03-03 21:29:41 +0200

    xml

---
+++ app/src/main/java/com/example/mobileappgroup8/AnalysisActivity.java
@@ -0,0 +1,13 @@
+package com.example.mobileappgroup8;
+
+import android.os.Bundle;
+
+public class AnalysisActivity extends MainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.analysis_activity);
+
+    }
+}
---
+++ app/src/main/java/com/example/mobileappgroup8/HistoryActivity.java
@@ -0,0 +1,13 @@
+package com.example.mobileappgroup8;
+
+import android.os.Bundle;
+
+public class HistoryActivity extends MainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.history_activity);
+
+    }
+}
